61c82c1fd4d353bc14c52c0a51a9264e09308bbc
Sylvain <rx.sylvain@gmail.com>
2016-05-03 15:53:32 -0400

    Added files via upload

---
+++ incron_bin/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#http://anionix.ddns.net/dists/jessie-64k/main/binary-armhf/
+
+dpkg -i htop_1.0.3-1_armhf.deb
+
+dpkg -i libgcc1-dbg_4.9.2-10_armhf.deb
+dpkg -i libstdc++6_4.9.2-10_armhf.deb
+dpkg -i libstdc++6-4.9-dbg_4.9.2-10_armhf.deb
+dpkg -i incron_0.5.10-2_armhf.deb
+
+dpkg -i libsystemd0_215-17+deb8u3_armhf.deb
+dpkg -i libminiupnpc10_1.9.20140610-2_armhf.deb
+dpkg -i libnatpmp1_20110808-3_armhf.deb
+dpkg -i transmission-cli_2.84-0.2_armhf.deb
+dpkg -i transmission-common_2.84-0.2_all.deb
+dpkg -i transmission-daemon_2.84-0.2_armhf.deb
