ba0542257f8bcc397a073ba2859f7e5c798484dc
Sylvain <rx.sylvain@gmail.com>
2016-05-18 13:02:32 -0400

    Add files via upload

--- sortmedia_scripts/sortmediafile.sh
+++ sortmedia_scripts/sortmediafile.sh
@@ -11,81 +11,90 @@ PATH_MOVIES="$4"
 PATH_LOG="$5"
 PID="$6"
 
-#TODO : favorize score for a tvshow folder if keywords are coming in the right order.
+#TODO : favorize score for a tvshow folder if keywords are coming in its name the right order.
+#TODO : if no season indicator on the file, check for its parent directory.
+#TODO : if no keywords, check for parent directory
+#TODO : try the last destination directory as a first candidate to sort the current file into.
 
 shopt -s nocaseglob
 
-if [[ $FILENAME != .* ]] && [[ $FILENAME != */.* ]] && [[ $FILENAME != *.torrent ]] && [[ $FILENAME != *.txt ]] && [[ $FILENAME != *.nfo ]] && [[ $FILENAME != *sample* ]] && [[ "$FILENAME" ]]; then
-	#Extract keywords and episode position in the tvshow
-	KEYWORDS=$FILENAME
-	KEYWORDS=$(basename "$KEYWORDS")
-	KEYWORDS="${KEYWORDS%.*}"
-	KEYWORDS=$(echo "$KEYWORDS" | sed -r 's/[\._-]+/\ /g') 		#replace . _ - with spaces
-	KEYWORDS=$(echo "$KEYWORDS" | sed -e 's/\[[^][]*\]//g')		#remove brackets with their content
-	EPISODE_ID=$([[ "$KEYWORDS" =~ [S|s][0-9]+[E|e][0-9]+|[S|s]eason\ *[0-9]+|[0-9]+x[0-9]+ ]] && echo $BASH_REMATCH)
-	KEYWORDS=$(echo "$KEYWORDS" | sed -e 's/[S|s]eason\ *[0-9]*.*//g')
-	KEYWORDS=$(echo "$KEYWORDS" | sed -e 's/[S|s][0-9]*[E|e][0-9]*.*//g')
-        KEYWORDS=$(echo "$KEYWORDS" | sed -e 's/[0-9]x[0-9]*.*//g')
-	EPISODE_SEASON=$([[ $EPISODE_ID =~ [0-9]+ ]] && echo $BASH_REMATCH)
-	EPISODE_SEASON=${EPISODE_SEASON#0}
-        KEYWORDS=$(echo "${KEYWORDS,,}")
-        KEYWORDS=$(echo "${KEYWORDS~}")
-	KEYWORDS="$(echo -e "${KEYWORDS}" | sed -e 's/[[:space:]]*$//')"
+[[ $FILENAME == .* ]] && exit 0
+[[ $FILENAME == */.* ]] && exit 0
+[[ $FILENAME == *.torrent ]] && exit 0
+[[ $FILENAME == *.txt ]] && exit 0
+[[ $FILENAME == *.nfo ]] && exit 0
+[[ $FILENAME =~ .*[S|s]ample.* ]] && exit 0
 
-	#Sort as a movie if there is no episode id in the file name
-	if [[ -z $EPISODE_SEASON ]]; then
-		echo "($PID) CREATING : $PATH_MOVIES/$KEYWORDS" >> "${PATH_LOG}/sort.log"
-		mkdir "$PATH_MOVIES/$KEYWORDS"
-		PATH_DEST="$PATH_MOVIES/$KEYWORDS"
-	        echo "($PID) MOVING :  $PATH_REPOSITORY/$FILENAME" >> "${PATH_LOG}/sort.log"
-	        echo "($PID) TO :      $PATH_DEST/" >> "${PATH_LOG}/sort.log"
-	        mv "$PATH_REPOSITORY/$FILENAME" "$PATH_DEST/" >> "$PATH_LOG/sort.log"
-		chmod 777 -R "$PATH_DEST"
-		exit 1
-	fi
+#Extract keywords and episode position in the tvshow
+KEYWORDS=$FILENAME
+KEYWORDS=$(basename "$KEYWORDS")
+KEYWORDS="${KEYWORDS%.*}"
+KEYWORDS=$(echo "$KEYWORDS" | sed -r 's/[\._-]+/\ /g') 		#replace . _ - with spaces
+KEYWORDS=$(echo "$KEYWORDS" | sed -r "s/[']+//g") 		#remove '
+KEYWORDS=$(echo "$KEYWORDS" | sed -e 's/\[[^][]*\]//g')		#remove brackets with their content
+EPISODE_ID=$([[ "$KEYWORDS" =~ [S|s][0-9]+[E|e][0-9]+|[S|s]eason\ *[0-9]+|[0-9]+x[0-9]+ ]] && echo $BASH_REMATCH)
+KEYWORDS=$(echo "$KEYWORDS" | sed -e 's/[S|s]eason\ *[0-9]*.*//g')
+KEYWORDS=$(echo "$KEYWORDS" | sed -e 's/[S|s][0-9]*[E|e][0-9]*.*//g')
+KEYWORDS=$(echo "$KEYWORDS" | sed -e 's/[0-9]x[0-9]*.*//g')
+EPISODE_SEASON=$([[ $EPISODE_ID =~ [0-9]+ ]] && echo $BASH_REMATCH)
+EPISODE_SEASON=${EPISODE_SEASON#0}
+KEYWORDS=$(echo "${KEYWORDS,,}")
+KEYWORDS=$(echo "${KEYWORDS~}")
+KEYWORDS="$(echo -e "${KEYWORDS}" | sed -e 's/[[:space:]]*$//')"
 
-	#Match keywords with TVSHOWS #
-	declare -A SCORES
-	for KEYWORD in $KEYWORDS; do
-		if ls "${PATH_TVSHOWS}" | grep -E *${KEYWORD}* 1> /dev/null 2>&1; then
-		for CURRENT_FILE in "${PATH_TVSHOWS}"/*${KEYWORD}*; do
+#Sort as a movie if there is no episode id in the file name
+if [[ -z $EPISODE_SEASON ]]; then
+#	echo "($PID) CREATING : $PATH_MOVIES/$KEYWORDS" >> "${PATH_LOG}/sort.log"
+#	mkdir "$PATH_MOVIES/$KEYWORDS"
+#	PATH_DEST="$PATH_MOVIES/$KEYWORDS"
+#	echo "($PID) MOVING :   ./$FILENAME" >> "${PATH_LOG}/sort.log"
+#	echo "($PID) TO :       $PATH_DEST" >> "${PATH_LOG}/sort.log"
+#	mv "$PATH_REPOSITORY/$FILENAME" "$PATH_DEST/" >> "$PATH_LOG/sort.log"
+#	chmod 777 -R "$PATH_DEST"
+	exit 1
+fi
+
+#Match keywords with TVSHOWS
+declare -A SCORES
+BESTSCORETVSHOW=""
+SECONDSCORE=0
+BESTSCORE=0
+for KEYWORD in $KEYWORDS; do
+	for CURRENT_FILE in "${PATH_TVSHOWS}"/*; do
+		if [[ $(echo "$CURRENT_FILE" | sed -r "s/[\'\"]+//g") ==  *${KEYWORD}* ]]; then
 			if [[ -z SCORES["$CURRENT_FILE"] ]]; then SCORES["$CURRENT_FILE"]=1;
 			else SCORES["$CURRENT_FILE"]=$((SCORES["$CURRENT_FILE"]+1)); fi
-		done
+			if [[ ${SCORES["$CURRENT_FILE"]} -ge $BESTSCORE ]]; then
+				SECONDSCORE=$BESTSCORE
+				BESTSCORE=$((SCORES["$CURRENT_FILE"]))
+				BESTSCORETVSHOW="$CURRENT_FILE"
+			fi
 		fi
 	done
-	BESTSCORETVSHOW=""
-	SECONDSCORE=0
-	BESTSCORE=0
-	for SCORE in "${!SCORES[@]}"; do
-		if [[ ${SCORES[${SCORE[$i]}]} -ge $BESTSCORE ]]; then
-			SECONDSCORE=$BESTSCORE
-			BESTSCORE=${SCORES[${SCORE[$i]}]}
-			BESTSCORETVSHOW="${SCORE[$i]}"
-		fi
-	done
-	unset -v "SCORES"
+done
+unset -v "SCORES"
 
-	#Verify if destination directories exist or create them
-	if [[ $BESTSCORE -eq $SECONDSCORE ]]; then
-		echo "($PID) CREATING : $PATH_TVSHOWS/$KEYWORDS" >> "${PATH_LOG}/sort.log"
-                mkdir "$PATH_TVSHOWS/$KEYWORDS"
-		BESTSCORETVSHOW="$PATH_TVSHOWS/$KEYWORDS"
-	fi
-	PATH_DEST="$BESTSCORETVSHOW/Season $EPISODE_SEASON"
-	if [[ ! -d "$BESTSCORETVSHOW/Season $EPISODE_SEASON" ]]; then
-		if [[ ! -d "$BESTSCORETVSHOW/season $EPISODE_SEASON" ]]; then
-			echo "($PID) CREATING : $BESTSCORETVSHOW/Season $EPISODE_SEASON" >> "${PATH_LOG}/sort.log"
-			mkdir "$BESTSCORETVSHOW/Season $EPISODE_SEASON"
-		else
-			PATH_DEST="$BESTSCORETVSHOW/season $EPISODE_SEASON";
-		fi
-	fi
-	echo "Destination: ${PATH_DEST}";
-
-	#Fetch and move file to its destination
-	echo "($PID) MOVING :  $PATH_REPOSITORY/$FILENAME" >> "${PATH_LOG}/sort.log"
-	echo "($PID) TO :      $PATH_DEST/" >> "${PATH_LOG}/sort.log"
-	mv "$PATH_REPOSITORY/$FILENAME" "$PATH_DEST/"
-	chmod 777 "$PATH_DEST/$FILENAME"
+#Verify if destination directories exist or create them
+if [[ $BESTSCORE -eq $SECONDSCORE ]]; then
+	echo "($PID) CREATING : $PATH_TVSHOWS/$KEYWORDS" >> "${PATH_LOG}/sort.log"
+	mkdir "$PATH_TVSHOWS/$KEYWORDS"
+	chmod 777 "$PATH_TVSHOWS/$KEYWORDS"
+	BESTSCORETVSHOW="$PATH_TVSHOWS/$KEYWORDS"
 fi
+PATH_DEST="$BESTSCORETVSHOW/Season $EPISODE_SEASON"
+if [[ ! -d "$BESTSCORETVSHOW/Season $EPISODE_SEASON" ]]; then
+	if [[ ! -d "$BESTSCORETVSHOW/season $EPISODE_SEASON" ]]; then
+		echo "($PID) CREATING : $BESTSCORETVSHOW/Season $EPISODE_SEASON" >> "${PATH_LOG}/sort.log"
+		mkdir "$BESTSCORETVSHOW/Season $EPISODE_SEASON"
+		chmod 777 "$BESTSCORETVSHOW/Season $EPISODE_SEASON"
+	else
+		PATH_DEST="$BESTSCORETVSHOW/season $EPISODE_SEASON";
+	fi
+fi
+
+#Fetch and move file to its destination
+echo "($PID) MOVING :   ./$FILENAME" >> "${PATH_LOG}/sort.log"
+echo "($PID) TO :       $PATH_DEST" >> "${PATH_LOG}/sort.log"
+mv "$PATH_REPOSITORY/$FILENAME" "$PATH_DEST/"
+chmod 777 "$PATH_DEST/$FILENAME"
+exit 0
--- sortmedia_scripts/sortmedias.sh
+++ sortmedia_scripts/sortmedias.sh
@@ -17,51 +17,64 @@ PATH_REPOSITORY="$2"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TIMEOUT=60
 
-if [[ $1 != .* ]] && [[ $1 != *.torrent ]] && [[ $1 != *.part ]] && [[ $1 != *__??????  ]] && [[ "$1" ]]; then
-	echo "($$) $(date +%Y-%m-%d\ %H:%M:%S)  file: $1  repository: $2  event: $3($4)" >> "${PATH_LOG}/sort.log"
-	#Handle directory
-	if [[ $3 == *IN_ISDIR* ]]; then
-		DIRNAME=$1
-                #Wait if files are downloading
-                while [[ $(inotifywait -c -t 2 -e modify "$PATH_REPOSITORY/$DIRNAME" ) == *MODIFY* ]]; do
-			NOTIF=$(inotifywait -c -t $TIMEOUT -e close_write "${PATH_REPOSITORY}"/"${DIRNAME}")
-			if [[ "$NOTIF" == *CLOSE_WRITE* ]]; then
-				FILENAME=$(echo $NOTIF | sed -r 's/.*,\".*\",//g')
-                	        "$DIR"/sortmediafile.sh "$DIRNAME/$FILENAME" "$PATH_REPOSITORY" "$PATH_TVSHOWS" "$PATH_MOVIES" "$PATH_LOG" "$$"
-			fi
-                done
-		#Check for existing files in the directory
-		if ls "$PATH_REPOSITORY/$DIRNAME/"* 1> /dev/null 2>&1; then
+shopt -s nocaseglob
+
+[[ $1 == .* ]] && exit 1
+[[ $1 == *.torrent ]] && exit 1
+[[ $1 == *.part ]] && exit 1
+[[ $1 == *__?????? ]] && exit 1
+[[ ! "$1" ]] && exit 1
+
+echo "($$) $(date +%Y-%m-%d\ %H:%M:%S)  file: $1  repository: $2  event: $3($4)" >> "${PATH_LOG}/sort.log"
+#Handle directory
+if [[ $3 == *IN_ISDIR* ]]; then
+	DIRNAME=$1
+	SUCCESS=1
+	#Wait if files are downloading
+	while [[ $(inotifywait -c -t 2 -e modify "$PATH_REPOSITORY/$DIRNAME" ) == *MODIFY* ]]; do
+		NOTIF=$(inotifywait -c -t $TIMEOUT -e close_write "${PATH_REPOSITORY}"/"${DIRNAME}")
+		if [[ "$NOTIF" == *CLOSE_WRITE* ]]; then
+			FILENAME=$(echo $NOTIF | sed -r 's/.*,\".*\",//g')
+			"$DIR"/sortmediafile.sh "$DIRNAME/$FILENAME" "$PATH_REPOSITORY" "$PATH_TVSHOWS" "$PATH_MOVIES" "$PATH_LOG" "$$"
+			if [ $? -eq 1 ]; then SUCCESS=0; fi
+		fi
+	done
+	#Check for existing files in the directory
+	if ls "$PATH_REPOSITORY/$DIRNAME/"* 1> /dev/null 2>&1; then
 		for FILENAME in "$PATH_REPOSITORY/$DIRNAME/"*; do
-                	FILENAME=$(echo $FILENAME | sed -e 's/.*\///g')
-      	        	"$DIR"/sortmediafile.sh "$DIRNAME/$FILENAME" "$PATH_REPOSITORY" "$PATH_TVSHOWS" "$PATH_MOVIES" "$PATH_LOG" "$$"
+			FILENAME=$(echo $FILENAME | sed -e 's/.*\///g')
+			"$DIR"/sortmediafile.sh "$DIRNAME/$FILENAME" "$PATH_REPOSITORY" "$PATH_TVSHOWS" "$PATH_MOVIES" "$PATH_LOG" "$$"
+			if [ $? -eq 1 ]; then SUCCESS=0; fi
 		done
-                fi
-		#Trash the directory
+	fi
+	#Trash the directory
+	if [[ ! $SUCCESS -eq 0 ]]; then
 		echo "($$) TRASHING : $PATH_REPOSITORY/$DIRNAME" >> "${PATH_LOG}/sort.log"
 		mv "$PATH_REPOSITORY/$DIRNAME" "$PATH_TRASH"
-
-	#Handle single file
-	else
-		FILENAME=$1
-		#Wait if the file is downloading
-		while [[ $(inotifywait -c -t 2 -e modify "$PATH_REPOSITORY/$FILENAME" ) == *MODIFY* ]]; do
-			NOTIF=$(inotifywait -c -t $TIMEOUT -e close_write "$PATH_REPOSITORY/$FILENAME")
-			if [[ "$NOTIF" == *CLOSE_WRITE* ]]; then break; fi
-		done
-		#when transfering a bunch of file at a time from the MacOS Finder to the mycloud
-		#some are created then closed without any data written into them and then recreated
-		#to be fully written. The following test prevent to sort those empty files.
-		FILESIZE=$(wc -c <"$PATH_REPOSITORY/$FILENAME")
-		if [[ $FILESIZE -eq 0 ]]; then
-			echo "($$) ERROR : EMPTY FILE" >> "$PATH_LOG/sort.log"
-			echo "($$) TRASHING : $PATH_REPOSITORY/$FILENAME" >> "${PATH_LOG}/sort.log"
-			mv "$PATH_REPOSITORY/$FILENAME" "$PATH_TRASH"
-			echo "($$) FAILED" >> "$PATH_LOG/sort.log"
-			exit 1
-		fi
-		"$DIR"/sortmediafile.sh "$FILENAME" "$PATH_REPOSITORY" "$PATH_TVSHOWS" "$PATH_MOVIES" "$PATH_LOG" "$$"
+		chmod 777 -R "$PATH_TRASH/$DIRNAME"
 	fi
 
-	echo "($$) DONE" >> "$PATH_LOG/sort.log"
+	#Handle single file
+else
+	FILENAME=$1
+	#Wait if the file is downloading
+	while [[ $(inotifywait -c -t 2 -e modify "$PATH_REPOSITORY/$FILENAME" ) == *MODIFY* ]]; do
+		NOTIF=$(inotifywait -c -t $TIMEOUT -e close_write "$PATH_REPOSITORY/$FILENAME")
+		if [[ "$NOTIF" == *CLOSE_WRITE* ]]; then break; fi
+	done
+	#when transfering a bunch of file at a time from the MacOS Finder to the mycloud
+	#some are created then closed without any data written into them and then recreated
+	#to be fully written. The following test prevent to sort those empty files.
+	FILESIZE=$(wc -c <"$PATH_REPOSITORY/$FILENAME")
+	if [[ $FILESIZE -eq 0 ]]; then
+		echo "($$) ERROR : EMPTY FILE" >> "$PATH_LOG/sort.log"
+		echo "($$) TRASHING : $PATH_REPOSITORY/$FILENAME" >> "${PATH_LOG}/sort.log"
+		mv "$PATH_REPOSITORY/$FILENAME" "$PATH_TRASH"
+		chmod 777 "$PATH_TRASH/$FILENAME"
+		echo "($$) FAILED" >> "$PATH_LOG/sort.log"
+		exit 1
+	fi
+	"$DIR"/sortmediafile.sh "$FILENAME" "$PATH_REPOSITORY" "$PATH_TVSHOWS" "$PATH_MOVIES" "$PATH_LOG" "$$"
 fi
+
+echo "($$) DONE" >> "$PATH_LOG/sort.log"
